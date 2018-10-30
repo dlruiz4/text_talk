dce1cc670a79ed384ac35b4021a04c29de4f88cd
dlruiz4 <44623807+dlruiz4@users.noreply.github.com>
2018-10-30 17:56:27 -0500

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,27 @@
+var cacheName = 'app-v2';
+var appShellFiles = [
+ "./index.html",
+ "./loading.gif"
+];
+
+import {MDCRipple} from '@material/ripple';
+
+
+
+
+self.addEventListener('fetch', function(e) {
+  e.respondWith(
+    caches.match(e.request).then(function(r) {
+      console.log('[Service Worker] Fetching resource: '+ e.request.url);
+  
+      
+      return r || fetch(e.request).then(function(response) {
+          return caches.open(cacheName).then(function(cache) {
+            console.log('[Service Worker] Caching new resource: '+e.request.url);
+            cache.put(e.request, response.clone());
+            return response;
+        });
+      });
+    })
+  );
+});
